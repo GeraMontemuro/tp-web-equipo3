7754f47214e1fe28ae6f6800db4c3af0cbf7eb52
roruiz93 <69327406+roruiz93@users.noreply.github.com>
2023-10-04 21:25:00 -0300

    primer intento

---
+++ Tp_WEB_Equipo_3/Tp_WEB_Equipo_3/dominio/Articulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        
+        public int IDArticulo { get; set; }
+        [DisplayName ("Código Artículo")]
+        public string CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+        public string UrlImagen { get; set; }
+        public Marca Marca { get; set; }
+        [DisplayName("Categoría")]
+        public Categoria Categoria{ get; set; }
+        public float Precio { get; set; }
+
+
+        public Articulo()
+        {
+            UrlImagen = "https://production.listennotes.com/podcasts/el-podcast-m%C3%A1s-random-del-mundo-L6I3Ep9lRTB-xB_PCg0EDch.1400x1400.jpg";
+        }
+    }
+
+}
