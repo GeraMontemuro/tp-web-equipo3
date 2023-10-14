86e0f070daa6c7f286f63796dc201a20f147505f
CrisMoreira2510 <106870383+CrisMoreira2510@users.noreply.github.com>
2023-10-13 23:01:38 -0300

    Incorporación de filtros marca, categoría

--- Tp_WEB_Equipo_3/Tp_WEB_Equipo_3/Mspage.Master.cs
+++ Tp_WEB_Equipo_3/Tp_WEB_Equipo_3/Mspage.Master.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using dominio;
+using negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,7 @@ namespace Tp_WEB_Equipo_3
 {
     public partial class Site1 : System.Web.UI.MasterPage
     {
+        public List<Articulo> ListaFiltro { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
             /* traer la lista de articulos 
@@ -17,6 +20,25 @@ namespace Tp_WEB_Equipo_3
              * pasar ese filtro no se de que manera el listado de default
              * mostrar desde default
              */
+            Marca marcasFiltro = new Marca();
+            MarcaNegocio marNegFiltro = new MarcaNegocio();
+            List<Marca>listaMarcas= new List<Marca>();
+
+            Categoria categoriaFiltro = new Categoria();
+            CategoriaNegocio marCatFiltro = new CategoriaNegocio();
+            List<Categoria> listaCategorias = new List<Categoria>();
+
+            listaMarcas = marNegFiltro.listar();
+            listaCategorias = marCatFiltro.listar();    
+
+            ddlMarca.DataSource = listaMarcas;
+            ddlMarca.DataValueField = "Descripcion";
+            ddlMarca.DataBind();
+
+            ddlCategoria.DataSource = listaCategorias;
+            ddlCategoria.DataValueField = "Descripcion";
+            ddlCategoria.DataBind();
+
         }
     }
 }
--- Tp_WEB_Equipo_3/Tp_WEB_Equipo_3/Mspage.Master.designer.cs
+++ Tp_WEB_Equipo_3/Tp_WEB_Equipo_3/Mspage.Master.designer.cs
@@ -33,6 +33,24 @@ namespace Tp_WEB_Equipo_3
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// ddlMarca control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlMarca;
+
+        /// <summary>
+        /// ddlCategoria control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategoria;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
